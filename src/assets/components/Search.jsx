82c9d107a3654d56793482bc17dcad9a4b8bce2f
Kavishka Dulshan <kavishkadulshan2@gmail.com>
2025-07-14 23:54:56 +0530

    improve search componnent

--- src/assets/components/Search.jsx
+++ src/assets/components/Search.jsx
@@ -4,6 +4,7 @@ const Search = ({searchTerm, setSearchTerm}) => {
   return (
     <div className='search'>
         <div>
+            <h1>Enter something here</h1>
             <input type="text" 
             placeholder='Enter something' 
             value={searchTerm}
