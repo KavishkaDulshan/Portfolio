de095ef7f28ec76360e250adbdd0a0561211a3f1
Kavishka Dulshan <kavishkadulshan2@gmail.com>
2025-07-16 21:12:23 +0530

    edit navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
-import { useState } from 'react';
+// import { useState } from 'react';
 import Search from './assets/components/Search'
 import NavBar from './assets/components/NavBar'
 
 const App = () => {
 
-  const [searchTerm,setSearchTerm] = useState('');
+  // const [searchTerm,setSearchTerm] = useState('');
 
   return (
     <div>
-      <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+      {/* <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/> */}
       <NavBar />
     </div>
   )
